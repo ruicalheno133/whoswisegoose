6d950cc6f1e0325942f2f67566815ec58b67a753
Rui Calheno <rcalheno@tripadvisor.com>
2025-06-05 22:40:34 +0100

    fix: BrowserRouter

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './screens/Home';
 import Game from './screens/Game';
 import Stats from './screens/Stats';
 
 export default function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/whoswisegoose">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
         <Route path="/stats" element={<Stats />} />
       </Routes>
-    </Router >
+    </BrowserRouter>
   );
 }
